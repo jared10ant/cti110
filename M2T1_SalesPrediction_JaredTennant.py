33e91ec2773946f75bde08137603de44e1a7709f
jared10ant <tennantj8722@student.faytechcc.edu>
2018-02-17 20:39:24 -0500

    Delete M2T1_SalesPrediction_JaredTennant.py

--- M2T1_SalesPrediction_JaredTennant.py
+++
@@ -1,9 +0,0 @@
-#Sales Prediction exercise
-#06/11/17
-#CTI-110 M2T1 - Sales Prediction
-#Jared Tennant
-
-total_sales = int(input('Enter the projected total sales: '))
-total_profit = total_sales * .23
-print(total_profit)
-
